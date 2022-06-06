d3ebf95c883df66ee93b04a9fa694c6568df2b34
lethexixin <lethexixin@163.com>
2022-06-06 19:21:27 +0800

    fix jwt

--- library/tools/mid_gin/jwt.go
+++ library/tools/mid_gin/jwt.go
@@ -2,13 +2,14 @@ package mid_gin
 
 import (
 	"errors"
+	"net/http"
 	"strings"
 	"time"
 )
 
 import (
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 )
 
 var (
@@ -20,6 +21,7 @@ var (
 
 // 教程地址: https://www.cnblogs.com/marshhu/p/12639633.html
 // CustomClaims 自定义声明结构体并内嵌jwt.StandardClaims
+// jwt.StandardClaims.ExpiresAt 设置过期时间，jwt.StandardClaims.Issuer 设置签发人
 // jwt包自带的jwt.StandardClaims只包含了官方字段
 // 我们这里需要额外记录下面的几个字段，所以要自定义结构体
 // 如果想要保存更多信息，都可以添加到这个结构体中
@@ -29,13 +31,14 @@ type CustomClaims struct {
 	jwt.StandardClaims
 }
 
-// 定义JWT的过期时间，这里以3天为例
-const TokenExpireDuration = time.Hour * 24 * 3
+// 定义JWT的过期时间，这里以1天为例
+const TokenExpireDuration = time.Hour * 24
 
 // 创建一个jwt token
 func CreateToken(claims CustomClaims, key []byte) (string, error) {
-	claims.StandardClaims.ExpiresAt = time.Now().Add(TokenExpireDuration).Unix() // 过期时间,3天
-	claims.StandardClaims.Issuer = "go-gin-model"                                // 签发人
+	if claims.StandardClaims.ExpiresAt == 0 {
+		claims.StandardClaims.ExpiresAt = time.Now().Add(TokenExpireDuration).Unix() // 过期时间,1天
+	}
 	// 使用指定的签名方法创建签名对象
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	// 使用指定的key签名并获得完整的编码后的字符串token
@@ -84,8 +87,7 @@ func JWT(key []byte) gin.HandlerFunc {
 		// token := c.Query("token")
 		token := c.Request.Header.Get("Authorization")
 		if token == "" {
-			//todo
-			//web.Fail(c, web.HandlerAuthIsEmptyCode)
+			c.String(http.StatusOK, "Authorization Token is empty")
 			c.Abort()
 			return
 		}
@@ -93,8 +95,7 @@ func JWT(key []byte) gin.HandlerFunc {
 		// 按空格分割
 		parts := strings.SplitN(token, " ", 2)
 		if !(len(parts) == 2 && parts[0] == "Bearer") {
-			//todo
-			//web.Fail(c, web.HandlerAuthIsErrCode)
+			c.String(http.StatusOK, "Authorization Bearer is empty")
 			c.Abort()
 			return
 		}
@@ -102,14 +103,13 @@ func JWT(key []byte) gin.HandlerFunc {
 		// parts[1]是获取到的tokenString，我们使用之前定义好的解析JWT的函数来解析它
 		claims, err := ParseToken(parts[1], key)
 		if err != nil {
-			//todo
-			//web.Fail(c, web.TokenIsInvalidCode)
+			c.String(http.StatusOK, "Token is invalid")
 			c.Abort()
 			return
 		}
 
 		// 将当前请求的用户信息保存到请求的上下文c上
 		c.Set("users", claims)
-		c.Next() // 后续的处理函数可以用过c.Get("claims") 之类的方法来获取当前请求的用户信息
+		c.Next() // 后续的处理函数可以用过c.Get("users") 之类的方法来获取当前请求的用户信息
 	}
 }
