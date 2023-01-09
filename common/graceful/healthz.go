df4f360f20846c4569db08ed93e60065f03a8aca
lethexixin <lethexixin@163.com>
2023-01-09 15:50:56 +0800

    optimize graceful

--- common/graceful/healthz.go
+++ common/graceful/healthz.go
@@ -22,6 +22,14 @@ func handlerUpListen() {
 			w.WriteHeader(http.StatusOK)
 		}
 		http.HandleFunc("/"+healthzInfo.router, h)
+
+		s := func(w http.ResponseWriter, _ *http.Request) {
+			logger.Infof("preStop received")
+			// 程序退出, 将 healthz 置为false, k8s的就绪检测才不会将流量分配到该程序
+			unHealthz()
+		}
+		http.HandleFunc("/stop", s)
+
 		if err := http.ListenAndServe(":"+healthzInfo.port, nil); err != nil {
 			logger.Errorf("start healthz check err:%s", err.Error())
 		}
